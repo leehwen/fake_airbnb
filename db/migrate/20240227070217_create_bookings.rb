74e2ff4e1d3dedb72b63d1d6f6e520d99e767a70
Yong Feng <fengrenaaa@gmail.com>
2024-02-27 15:23:35 +0800

    fixed model association

--- db/migrate/20240227070217_create_bookings.rb
+++ db/migrate/20240227070217_create_bookings.rb
@@ -4,7 +4,7 @@ class CreateBookings < ActiveRecord::Migration[7.1]
       t.date :start_date
       t.date :end_date
       t.integer :no_of_guests
-      t.boolean :host_approve
+      t.boolean :host_approved
       t.references :listing, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_27_070217) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_27_071819) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,7 +46,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_27_070217) do
     t.date "start_date"
     t.date "end_date"
     t.integer "no_of_guests"
-    t.boolean "host_approve"
+    t.boolean "host_approved"
     t.bigint "listing_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -76,6 +76,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_27_070217) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
