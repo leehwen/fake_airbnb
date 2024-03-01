901708a566287b4ca4aba19b280cfa74fb1f9d0f
ng <ng_zi_jie@hotmail.com>
2024-03-01 16:52:49 +0800

    css completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def dashboard
-    @bookings = current_user.bookings_as_host
+    @confirmed_bookings = current_user.bookings_as_host.where(host_approved: true)
+    @rejected_bookings = current_user.bookings_as_host.where(host_approved: false)
+    @pending_bookings = current_user.bookings_as_host.where(host_approved: nil)
     @listings = current_user.listings
   end
 end
